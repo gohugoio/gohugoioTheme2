e65e854ffa478edd485fe640c27aa06ec072c135
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2022-03-15 10:03:22 +0100

    Add search
    Fixes #21

--- assets/js/main/index.js
+++ assets/js/main/index.js
@@ -3,6 +3,7 @@ import { newNavStore, initColorScheme, bridgeTurboAndAlpine } from "./nav/index"
 import Alpine from "jslibs/alpinejs/v3/alpinejs/dist/module.esm.js";
 import intersect from "jslibs/alpinejs/v3/intersect/dist/module.esm.js";
 import persist from "jslibs/alpinejs/v3/persist/dist/module.esm.js";
+import focus from "jslibs/alpinejs/v3/focus/dist/module.esm.js";
 
 // Client state.
 (function () {
@@ -24,13 +25,21 @@ import persist from "jslibs/alpinejs/v3/persist/dist/module.esm.js";
 (function () {
   // Register AlpineJS plugins.
   {
+    Alpine.plugin(focus);
     Alpine.plugin(intersect);
     Alpine.plugin(persist);
   }
 
   // Register AlpineJS controllers.
   {
-    // None for now.
+    // Register AlpineJS data controllers.
+    let searchConfig = {
+      index: "hugodocs",
+      app_id: "BH4D9OD16A",
+      api_key: "167e7998590aebda7f9fedcf86bc4a55",
+    };
+
+    Alpine.data("searchController", () => newSearchController(searchConfig));
   }
 
   // Set up AlpineJS stores.
@@ -47,3 +56,75 @@ import persist from "jslibs/alpinejs/v3/persist/dist/module.esm.js";
   // Start the Turbo-Alpine bridge.
   bridgeTurboAndAlpine(Alpine);
 })();
+
+// TODO(bep) move out into its own file, also pull params out into config.
+function newSearchController(cfg) {
+  const groupByLvl0 = (array) => {
+    if (!array) return [];
+    return array.reduce((result, currentValue) => {
+      (result[currentValue.hierarchy.lvl0] = result[currentValue.hierarchy.lvl0] || []).push(currentValue);
+      return result;
+    }, {});
+  };
+
+  return {
+    query: "",
+    open: false,
+    result: {},
+    toggleOpen: function () {
+      this.open = !this.open;
+      this.checkOpen();
+    },
+    checkOpen: function () {
+      if (!this.open) {
+        return;
+      }
+      this.search();
+      this.$nextTick(() => {
+        this.$refs.input.focus();
+      });
+    },
+
+    init: function () {
+      this.checkOpen();
+      return this.$nextTick(() => {
+        this.$watch("query", () => {
+          this.search();
+        });
+      });
+    },
+    search: function () {
+      if (!this.query) {
+        this.result = {};
+        return;
+      }
+      var queries = {
+        requests: [
+          {
+            indexName: cfg.index,
+            params: `query=${encodeURIComponent(this.query)}`,
+            attributesToHighlight: ["hierarchy", "content"],
+            attributesToRetrieve: ["hierarchy", "url", "content"],
+          },
+        ],
+      };
+
+      const host = `https://${cfg.app_id}-dsn.algolia.net`;
+      const url = `${host}/1/indexes/*/queries`;
+
+      fetch(url, {
+        method: "POST",
+        headers: {
+          "X-Algolia-Application-Id": cfg.app_id,
+          "X-Algolia-API-Key": cfg.api_key,
+        },
+        body: JSON.stringify(queries),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          this.result = groupByLvl0(data.results[0].hits);
+          //console.log(this.result);
+        });
+    },
+  };
+}
